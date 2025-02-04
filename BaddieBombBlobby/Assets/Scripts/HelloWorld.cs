8ffa6cf3c237045f15a61dd79af2984a5013fce8
nawafh1 <144995796+nawafh1@users.noreply.github.com>
2025-02-03 19:00:50 -0700

    SQL Connection

---
+++ BaddieBombBlobby/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Print "Hello World" to the Unity Console
+        Debug.Log("Hello World");
+    }
+}
